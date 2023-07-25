9aa026ddd0a288341e6d6f9db7e4b6ff1d9f380b
mgregerson <mgregerson4321@gmail.com>
2023-07-25 14:22:30 -0400

    Completed two 5 KYU String questions

---
+++ Strings/weirdStringCase.py
@@ -0,0 +1,29 @@
+# Write a function toWeirdCase (weirdcase in Ruby) that accepts a string, and 
+# returns the same string with all even indexed characters in each word upper cased, 
+# and all odd indexed characters in each word lower cased. The indexing just explained 
+# is zero based, so the zero-ith index is even, therefore that character should be 
+# upper cased and you need to start over for each word.
+
+# The passed in string will only consist of alphabetical characters and spaces(' ').
+# Spaces will only be present if there are multiple words. 
+# Words will be separated by a single space(' ').
+
+# Examples:
+# to_weird_case('String'); # => returns 'StRiNg'
+# to_weird_case('Weird string case') # => returns 'WeIrD StRiNg CaSe'
+
+def to_weird_case(words):
+    # split the sentence into words in an array
+    split_words = words.split()
+
+    result = ''
+
+    for word in split_words:
+        weird_word = ''
+        for i, letter in enumerate(word):
+            if i % 2 == 0:
+                weird_word += letter.capitalize()
+            else:
+                weird_word += letter.lower()
+        result += weird_word + ' '
+    return result[:-1]
